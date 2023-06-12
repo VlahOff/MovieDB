ce6f84a8eff17734ce9f477fe37611b57111064b
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-06-12 15:01:09 +0300

    .

--- src/services/searchForMovie.jsx
+++ src/services/searchForMovie.jsx
@@ -1,5 +1,5 @@
 export const fetchMovie = async (title, type, year) => {
-  const response = await fetch(`http://www.omdbapi.com/?apikey=${
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${
       import.meta.env.VITE_OMDB_KEY
     }&t=${title}&type=${type}&y=${year}&r=json&plot=full`
   );
